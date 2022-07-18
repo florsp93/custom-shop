0f679da8492067b1b03a626f8d649104961d161e
Florencia Spina <florencia.spina93@gmail.com>
2022-07-18 20:16:23 -0300

    migrating to redux saga

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,7 +1,6 @@
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
 import { createAction } from "../../utils/reducer/reducer.utils";
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 export const fetchCategoriesStart = () => {
   console.log("fetch start");
@@ -19,19 +18,4 @@ export const fetchCategoriesSuccess = (categoriesArray) => {
 export const fetchCategoriesFailed = (error) => {
   console.log("fetch error");
   return createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
-};
-
-//THUNK ACTION > redux thunk recomienda declarar las thunk action con la terminacion ASYNC
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  console.log("fetch async");
-  dispatch(fetchCategoriesStart());
-
-  try {
-    const categoriesArray = await getCategoriesAndDocuments("categories");
-    console.log("fetch success");
-    dispatch(fetchCategoriesSuccess(categoriesArray));
-  } catch (error) {
-    console.log("fetch error");
-    dispatch(fetchCategoriesFailed(error));
-  }
 };
